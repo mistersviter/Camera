579e0bfc98d21dfa075ffa9afd2f7254d459acdb
Sviter <mistersviter@gmail.com>
2026-04-27 21:04:59 +0300

    check

--- src/config/camera.ts
+++ src/config/camera.ts
@@ -1,11 +1,11 @@
-import type { CaptureSettings, Resolution } from '../components/types'
+import type { CaptureSettings, Resolution } from '../components/types';
 
 export const CAMERA_REQUEST_RESOLUTION: Resolution = {
-  width: 3000,
-  height: 4000,
-}
+  width: 960,
+  height: 1280,
+};
 
 export const CAMERA_OUTPUT_SETTINGS: CaptureSettings = {
   width: 960,
   height: 1280,
-}
+};
